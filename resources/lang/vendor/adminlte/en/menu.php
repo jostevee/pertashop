5cd48cda237b6d2e138237264315625ad5833101
jostevee <steveeejooo@gmail.com>
2021-04-14 17:30:00 +0700

    Update Commit

---
+++ resources/lang/vendor/adminlte/en/menu.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'MAIN NAVIGATION',
+    'blog'                          => 'Blog',
+    'pages'                         => 'Pages',
+    'account_settings'              => 'ACCOUNT SETTINGS',
+    'profile'                       => 'Profile',
+    'change_password'               => 'Change Password',
+    'multilevel'                    => 'Multi Level',
+    'level_one'                     => 'Level 1',
+    'level_two'                     => 'Level 2',
+    'level_three'                   => 'Level 3',
+    'labels_menu'                   => 'MENU',
+    'labels'                        => 'LABELS',
+    'important'                     => 'Important',
+    'warning'                       => 'Warning',
+    'information'                   => 'Information',
+    'harga_produk'                  => 'Harga Produk',
+    'input_penerimaan_do'           => 'Input Penerimaan DO',
+    'input_arus_minyak'             => 'Input Arus Minyak',
+    'input_penerimaan'              => 'Input Penerimaan',
+    'input_penerimaan_t'            => 'Tunai',
+    'input_penerimaan_nt'           => 'Non Tunai',
+    'shift_settlement'              => 'Shift Settlement',
+    'input_setoran_tunai'           => 'Input Setoran Tunai',
+];
---
+++ resources/views/home.blade.php
@@ -0,0 +1,148 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+<h1>Dashboard</h1>
+@stop
+
+@section('content')
+<p>Selamat datang di Apps Pertashop. Untuk menu, silakan klik</p>
+<!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-4 col-8">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3><i class="fas fa-tag"></i></h3>
+
+                <p>Harga Produk</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-4 col-8">
+            <!-- small box -->
+            <div class="small-box bg-info"><!-- bg-success -->
+              <div class="inner">
+                <h3><i class="fas fa-truck"></i></h3>
+
+                <p>Input Penerimaan DO</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-4 col-8">
+            <!-- small box -->
+            <div class="small-box bg-info"><!-- bg-warning -->
+              <div class="inner">
+                <h3><i class="fas fa-oil-can"></i></h3>
+
+                <p>Input Arus Minyak</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-4 col-8">
+            <!-- small box -->
+            <div class="small-box bg-info"><!-- bg-danger -->
+              <div class="inner">
+                <h3><i class="fas fa-cash-register"></i></h3>
+
+                <p>Input Penerimaan Tunai dan Non Tunai</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-4 col-8">
+            <!-- small box -->
+            <div class="small-box bg-info"><!-- bg-danger -->
+              <div class="inner">
+                <h3><i class="fas fa-clipboard-list"></i></h3>
+
+                <p>Shift Settlement</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-4 col-8">
+            <!-- small box -->
+            <div class="small-box bg-info"><!-- bg-danger -->
+              <div class="inner">
+                <h3><i class="fas fa-university"></i></h3>
+
+                <p>Input Setoran Tunai</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
+      </div>
+    </section>
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+<script>
+    console.log('Hi!'); 
+</script>
+@stop
+
+@php
+/*
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+*/
+@endphp
